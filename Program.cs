7d0c882ecb82ec5caf93ef97e0b2a5a8eb145d28
Bedirhan <bedirhansiyam@gmail.com>
2023-03-11 19:03:19 +0400

    First commit

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿namespace MethodOverloading;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string number = "100";
+        int outNumber;
+        Methods sampleMethod = new Methods();
+
+        // Out Methods
+
+        bool result = int.TryParse(number, out outNumber);
+        if(result)
+        {
+            Console.WriteLine("Successful");
+            Console.WriteLine(outNumber);
+        }
+        else
+        {
+            Console.WriteLine("Unsuccessful");
+        }
+
+        sampleMethod.Sum(11, 24, out int resultSum);
+        Console.WriteLine(resultSum);
+
+
+        // Overloading Methods
+
+        sampleMethod.WriteToScreen("Hello World!");
+        sampleMethod.WriteToScreen(54);
+        sampleMethod.WriteToScreen("Hello", "World!");
+
+    }
+}
+
+class Methods
+{    
+    // Out Method
+    public void Sum(int a, int b, out int sum)
+    {
+        sum = a + b;
+    }
+
+    // Overloading Method
+    public void WriteToScreen(string data)
+    {
+        Console.WriteLine(data);
+    }
+    public void WriteToScreen(int data)
+    {
+        Console.WriteLine(data.ToString());
+    }
+    public void WriteToScreen(string data, string data1)
+    {
+        Console.WriteLine(data + " " + data1);
+    }
+}
